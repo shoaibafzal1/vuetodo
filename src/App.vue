85f0c9e1bd48c88ef0e57604c8d82c3cc955a6b0
= <=>
2020-11-16 20:29:26 +0500

    testing vuejs project todo app

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" v-on:delete-todo="deleteTodo" />
+    <AddTodo v-on:add-todo="addTodo" />
+  </div>
+</template>
+<script>
+import AddTodo from './components/AddTodo.vue';
+import Todos from './components/Todos';
+export default {
+  name: 'app',
+  components:{
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos:[
+        {
+          id:1,
+          title:'first task',
+          completed:false
+        },
+
+        {
+          id:2,
+          title:'do Laundary',
+          completed:false
+        },
+        {
+          id:3,
+          title:'make arrangement for living',
+          completed:false
+        }
+      ],
+    }
+  },
+
+  methods: {
+    addTodo(newTodoObj) {
+      this.todos = [...this.todos, newTodoObj]
+    },
+    deleteTodo(todoId) {
+      this.todos = this.todos.filter(todo => todo.id !== todoId);
+    }
+  }
+}
+</script>
